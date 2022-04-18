c21f059105526b1435abc774e0d2416813259551
Tauhid8k <tauhid8k@gmail.com>
2022-04-18 06:43:17 +0600

    redux-toolkit setup & basic products states

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,48 @@
-import { useState, useEffect } from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Col, Row, Spinner } from 'react-bootstrap';
 import Product from '../components/Product';
 import axios from 'axios';
+import { productActions } from '../redux/productSlice';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const { loading, products, error } = useSelector((state) => state.product);
+
+  console.log(error);
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
+      try {
+        const { data } = await axios.get('/api/products');
+        dispatch(productActions.getAllProducts(data));
+      } catch (error) {
+        dispatch(productActions.error(error.message));
+      }
     };
+
     fetchProducts();
-  }, []);
+  }, [dispatch]);
+
+  console.log(error);
 
   return (
     <>
       <h1>Latest Products</h1>
       <hr />
-      <Row>
-        {products.map((product) => (
-          <Col md={6} lg={4} xxl={3} key={product._id} className='d-flex'>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col md={6} lg={4} xxl={3} key={product._id} className='d-flex'>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
